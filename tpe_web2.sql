b242036463357ae1d0c8da80410417949c78e757
SEBASTIANULIBARRI <145642465+SEBASTIANULIBARRI@users.noreply.github.com>
2024-10-15 00:20:29 -0300

    Corrección base de datos

--- tpe_web2.sql
+++ tpe_web2.sql
@@ -1,42 +1,43 @@
-create database if not exists DB_TPE;
-Use DB_TPE;
+CREATE DATABASE IF NOT EXISTS db_tpe;
+USE db_tpe;
 
-CREATE TABLE if not exists `Usuarios` (
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `id_usuario` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `Usuario` varchar(150) NOT NULL,
-  `password` varchar(150) NOT NULL
-) ;
+  `usuario` varchar(150) NOT NULL UNIQUE,
+  `password` varchar(60) NOT NULL
+);
 
-INSERT INTO `Usuarios` (Usuario,password) VALUES
-("webadmin","admin");
 
-CREATE TABLE if not exists `Generos` (
+INSERT INTO `usuarios` (usuario, password) VALUES
+("webadmin", "$2y$10$.k09NI/.OMdOWsk9D76JJ./o6Wy7dJsbFnYWYLo8C3Ox44yHmfEXC");
+
+CREATE TABLE IF NOT EXISTS `generos` (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `Nombre` varchar(150) NOT NULL,
-  `Descripcion` varchar(255) NOT NULL,
-  `Ruta_Imagen` varchar(255)
-) ;
+  `nombre` varchar(150) NOT NULL UNIQUE,
+  `descripcion` varchar(255) NOT NULL,
+  `ruta_imagen` varchar(255)
+);
 
-INSERT INTO `Generos` (Nombre, Descripcion) VALUES 
-('Realismo', 'Narrativas que retratan la vida cotidiana y las experiencias humanas de manera fiel y veraz.'),
-('Novela', 'Obra literaria extensa que narra una historia de ficción con personajes y tramas desarrolladas.'),
-('Distopía', 'Relatos que presentan sociedades futuras caracterizadas por opresión y deshumanización.'),
-('Romántico', 'Género que enfatiza las emociones, el amor y la belleza de las relaciones humanas.');
 
-CREATE TABLE if not exists  `Libros` (
+INSERT INTO `generos` (nombre, descripcion) VALUES 
+('realismo', 'narrativas que retratan la vida cotidiana y las experiencias humanas de manera fiel y veraz.'),
+('novela', 'obra literaria extensa que narra una historia de ficción con personajes y tramas desarrolladas.'),
+('distopía', 'relatos que presentan sociedades futuras caracterizadas por opresión y deshumanización.'),
+('romántico', 'género que enfatiza las emociones, el amor y la belleza de las relaciones humanas.');
+
+CREATE TABLE IF NOT EXISTS `libros` (
   `id_libro` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `titulo` varchar(150) NOT NULL,
   `autor` varchar(100) NOT NULL,
   `paginas` int(11) NOT NULL,
   `id_genero` int(11) NOT NULL
-) ;
+);
 
-ALTER TABLE `Libros`
-  ADD CONSTRAINT `fk_Generos_Libros` FOREIGN KEY (`id_Genero`) REFERENCES `Generos` (`id`); 
+ALTER TABLE `libros`
+  ADD CONSTRAINT `fk_generos_libros` FOREIGN KEY (`id_genero`) REFERENCES `generos` (`id`);
 
-
-INSERT INTO Libros ( titulo, autor, id_genero, paginas) VALUES
-( 'Cien años de soledad', 'Gabriel García Márquez', 1, 417),
-( 'Don Quijote de la Mancha', 'Miguel de Cervantes', 2, 863),
-( '1984', 'George Orwell', 3, 328),
-( 'Orgullo y prejuicio', 'Jane Austen', 4, 432);
+INSERT INTO `libros` (titulo, autor, id_genero, paginas) VALUES
+('cien años de soledad', 'gabriel garcía márquez', 1, 417),
+('don quijote de la mancha', 'miguel de cervantes', 2, 863),
+('1984', 'george orwell', 3, 328),
+('orgullo y prejuicio', 'jane austen', 4, 432);
