baadbdc86c6bf5917fe92af918bdeff3416e2c57
SEBASTIANULIBARRI <145642465+SEBASTIANULIBARRI@users.noreply.github.com>
2024-11-10 10:34:27 -0300

    Cambio de nombre en la BBDD

--- config.php
+++ config.php
@@ -2,6 +2,5 @@
 const MYSQL_HOST = 'localhost';
 const MYSQL_USER = 'root';
 const MYSQL_PASS = '';
-const MYSQL_DB = 'tpe99dasdasdadasdasdasdadasddsa';
-// const MYSQL_DB = 'BD_Biblioteca_Grupo_99';
+const MYSQL_DB = 'TPE_BD_Biblioteca_Grupo_99';
 ?>
